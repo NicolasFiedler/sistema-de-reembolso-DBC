740c2155984c67e18cb8c4867827a418bf70025b
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-04-20 18:07:48 -0300

    start

---
+++ refound-api/src/main/java/dbc/vemser/refoundapi/RefoundApiApplication.java
@@ -0,0 +1,13 @@
+package dbc.vemser.refoundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefoundApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefoundApiApplication.class, args);
+	}
+
+}
---
+++ refound-api/src/test/java/dbc/vemser/refoundapi/RefoundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dbc.vemser.refoundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefoundApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
