b7eb1c347a6e539f57a59df3e57d4faa608f13d8
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-04-21 15:20:16 -0300

    new schema

--- script/script.sql
+++ script/script.sql
@@ -1,7 +1,9 @@
+CREATE SCHEMA refund_db;
+
 -- -----------------------------------------------------
--- Table public.roles
+-- Table refund_db.roles
 -- -----------------------------------------------------
-CREATE TABLE public.roles (
+CREATE TABLE refund_db.roles (
   id_role SERIAL,
   role TEXT NOT NULL UNIQUE,
 
@@ -10,9 +12,9 @@ CREATE TABLE public.roles (
 
 
 -- -----------------------------------------------------
--- Table public.users
+-- Table refund_db.users
 -- -----------------------------------------------------
-CREATE TABLE public.users (
+CREATE TABLE refund_db.users (
   id_user SERIAL,
   name TEXT NOT NULL,
   email TEXT NOT NULL,
@@ -23,14 +25,14 @@ CREATE TABLE public.users (
   PRIMARY KEY (id_user),
   CONSTRAINT fk_users_roles
     FOREIGN KEY (id_role)
-    REFERENCES public.roles (id_role)
+    REFERENCES refund_db.roles (id_role)
 );
 
 
 -- -----------------------------------------------------
--- Table public.refund
+-- Table refund_db.refund
 -- -----------------------------------------------------
-CREATE TABLE public.refund (
+CREATE TABLE refund_db.refund (
   id_refound SERIAL,
   title TEXT NOT NULL,
   date DATE NOT NULL,
@@ -41,14 +43,14 @@ CREATE TABLE public.refund (
   PRIMARY KEY (id_refound),
   CONSTRAINT fk_refund_users
     FOREIGN KEY (id_user)
-    REFERENCES public.users (id_user)
+    REFERENCES refund_db.users (id_user)
 );
 
 
 -- -----------------------------------------------------
--- Table public.item
+-- Table refund_db.item
 -- -----------------------------------------------------
-CREATE TABLE public.item (
+CREATE TABLE refund_db.item (
   id_item SERIAL,
   name TEXT NOT NULL,
   date DATE NOT NULL,
@@ -59,53 +61,53 @@ CREATE TABLE public.item (
   PRIMARY KEY (id_item),
   CONSTRAINT fk_item_refund
     FOREIGN KEY (id_refound)
-    REFERENCES public.refund (id_refound)
+    REFERENCES refund_db.refund (id_refound)
 );
 
 -- -----------------------------------------------------
--- Insert public.roles
+-- Insert refund_db.roles
 -- -----------------------------------------------------
-INSERT INTO public.roles (role) VALUES ('ROLE_ADMIN');
-INSERT INTO public.roles (role) VALUES ('ROLE_FINANCEIRO');
-INSERT INTO public.roles (role) VALUES ('ROLE_GESTOR');
-INSERT INTO public.roles (role) VALUES ('ROLE_COLABORADOR');
+INSERT INTO refund_db.roles (role) VALUES ('ROLE_ADMIN');
+INSERT INTO refund_db.roles (role) VALUES ('ROLE_FINANCEIRO');
+INSERT INTO refund_db.roles (role) VALUES ('ROLE_GESTOR');
+INSERT INTO refund_db.roles (role) VALUES ('ROLE_COLABORADOR');
 
 -- -----------------------------------------------------
--- Insert public.users              =Bcrypt=
+-- Insert refund_db.users              =Bcrypt=
 -- -----------------------------------------------------
--- INSERT INTO public.users (name, email, password, id_role) --password: admin
+-- INSERT INTO refund_db.users (name, email, password, id_role) --password: admin
 -- VALUES ('admin', 'admin', '$2a$12$R7zbqGcvuqVhvMKsQqQAXOH.goaNseEInEF2NwmuVM5acRzlQZLJO', 1);
 
--- INSERT INTO public.users (name, email, password, id_role) --password: financeiro
+-- INSERT INTO refund_db.users (name, email, password, id_role) --password: financeiro
 -- VALUES ('jonas', 'financeiro@dbccompany.com.br', '$2a$12$AANgLSu/127rSwlsodfrh.ZOL61Yzeg6c0wvtFs8n2oy3yLR7DAnO', 2);
 
--- INSERT INTO public.users (name, email, password, id_role) --password: gestor
+-- INSERT INTO refund_db.users (name, email, password, id_role) --password: gestor
 -- VALUES ('jaqueline', 'gestor@dbccompany.com.br', '$2a$12$Yx5jlNcOfLeWG3MMNdDfquM9wN4ShEgHFdYjP/Rdiw3ZHXW/T9zl6', 3);
 
--- INSERT INTO public.users (name, email, password, id_role) --password: 123
+-- INSERT INTO refund_db.users (name, email, password, id_role) --password: 123
 -- VALUES ('marcos', 'marcos.alves@dbccompany.com.br', '$2a$12$U.0QlYm2JSuWAt.C4.nP.O3Oy9qgFHYW7BIvfplH2Hz61z1DE1iJO', 4);
 
 -- -----------------------------------------------------
--- Insert public.users            =noBcritp=
+-- Insert refund_db.users            =noBcritp=
 -- -----------------------------------------------------
-INSERT INTO public.users (name, email, password, id_role)
+INSERT INTO refund_db.users (name, email, password, id_role)
 VALUES ('admin', 'admin', 'admin', 1);
 
-INSERT INTO public.users (name, email, password, id_role)
+INSERT INTO refund_db.users (name, email, password, id_role)
 VALUES ('jonas', 'financeiro@dbccompany.com.br', 'financeiro', 2);
 
-INSERT INTO public.users (name, email, password, id_role)
+INSERT INTO refund_db.users (name, email, password, id_role)
 VALUES ('jaqueline', 'gestor@dbccompany.com.br', 'gestor', 3);
 
-INSERT INTO public.users (name, email, password, id_role)
+INSERT INTO refund_db.users (name, email, password, id_role)
 VALUES ('marcos', 'marcos.alves@dbccompany.com.br', '123', 4);
 
 -- -----------------------------------------------------
--- Insert public.refund
+-- Insert refund_db.refund
 -- -----------------------------------------------------
 
 
 -- -----------------------------------------------------
--- Insert public.item
+-- Insert refund_db.item
 -- -----------------------------------------------------
 
