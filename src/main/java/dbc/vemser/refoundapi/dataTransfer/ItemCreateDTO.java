ec6f520dd3a391edc20f268515996bc8b0a0c886
NicolasFiedler <nicolasxfiedler@gmail.com>
2022-04-21 17:33:02 -0300

    fix error messege

--- src/main/java/dbc/vemser/refoundapi/dataTransfer/ItemCreateDTO.java
+++ src/main/java/dbc/vemser/refoundapi/dataTransfer/ItemCreateDTO.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-<<<<<<< HEAD
-import javax.persistence.Lob;
 import javax.validation.constraints.DecimalMin;
-=======
->>>>>>> 6aaaa953a4c83f0424b939cf29155fa9daca70c0
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -21,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ItemCreateDTO {
     @NotEmpty
-    @Size(min = 2, max = 60 , message = "o nome deve conter entre 2 a 40 caracteres")
+    @Size(min = 2, max = 60 , message = "o nome deve conter entre 2 a 60 caracteres")
     private String name;
 
     private LocalDateTime date;
