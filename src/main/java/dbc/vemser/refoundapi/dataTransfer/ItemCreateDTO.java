48f0be585972609776f484c1eb205d6a5a9a9eec
flavio <flaviosguian@gmail.com>
2022-04-21 16:57:33 -0300

    fix: limitações nas validacoes do DTO

--- src/main/java/dbc/vemser/refoundapi/dataTransfer/ItemCreateDTO.java
+++ src/main/java/dbc/vemser/refoundapi/dataTransfer/ItemCreateDTO.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Lob;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,10 +18,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ItemCreateDTO {
     @NotEmpty
+    @Size(min = 2, max = 60 , message = "o nome deve conter entre 2 a 40 caracteres")
     private String name;
 
     private LocalDateTime data;
     @NotNull
+    @DecimalMin(value = "0.01", message = "Não é permitido números negativos.")
     private Double value;
     private byte[] image;
 }
--- src/main/java/dbc/vemser/refoundapi/dataTransfer/RefundCreateDTO.java
+++ src/main/java/dbc/vemser/refoundapi/dataTransfer/RefundCreateDTO.java
@@ -6,14 +6,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 public class RefundCreateDTO {
+    @NotBlank
+    @Size(min = 2, max = 60 , message = "o titulo de reembolso deve conter entre 2 a 40 caracteres")
     private String title;
     private LocalDateTime data;
+    @NotNull
     private Status status;
+    @NotNull
+    @DecimalMin(value = "0.01", message = "Não é permitido números negativos.")
     private Double value;
 }
