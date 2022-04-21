44d2e81cf932f2693e3f725d4efc253ab97851ec
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-04-20 22:47:10 -0300

    add router.tsx file and AuthDTO

--- refound-web/src/index.tsx
+++ refound-web/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Routers from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Routers />
   </React.StrictMode>
 );
